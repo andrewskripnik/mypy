33381f10c9d44293549f8937cd4da70af46e2493
SkAI <andrewskripnik@gmail.com>
2024-12-18 22:20:23 +0200

    Modify python

--- addr_book/add_book.py
+++ addr_book/add_book.py
@@ -1,37 +1,37 @@
 from crud import search, add, remove
 
-act = raw_input('Choose action: \nsearch \nadd \ndelete \ngetDB \n')
+act = input('Choose action: \nsearch \nadd \ndelete \ngetDB \n')
 add_book = {}
 
 
 ######## Main
 def main():
   if act.lower() in ['se', 'sea', 'sear', 'searc', 'search']:
-      sname = raw_input('Name for search please: ')
+      sname = input('Name for search please: ')
       search(sname)
 
   elif act.lower() == 'add':
-      aname = raw_input('Name for update please: ')
-      aphone = raw_input('Phone for update please: ')
+      aname = input('Name for update please: ')
+      aphone = input('Phone for update please: ')
       try:
           add(aname, aphone)
       except ValueError:
-          print 'Contact exists'
+          print('Contact exists')
 
   elif act.lower() in ['delete','del']:
-      dname = raw_input('Exact name for deletion please: ')
+      dname = input('Exact name for deletion please: ')
       try:
           remove(dname)
       except ValueError:
-          print 'No such contact'
+          print('No such contact')
 
   elif act == 'getDB':
       try:
-          print "getDB()"
+          print("getDB()")
       except ValueError:
-          print ValueError
+          print(ValueError)
 
-  else: print "Choose action: se[arch], add, del[ete], getDB "
+  else: print("Choose action: se[arch], add, del[ete], getDB ")
 
 
 if __name__ == "__main__":
--- addr_book/crud.py
+++ addr_book/crud.py
@@ -1,5 +1,5 @@
 import re
-import ConfigParser
+import configparser
 
 ######## Functions
 
@@ -8,8 +8,7 @@ def search(param):
   add_book = db_w.getDB()
   for key, val in add_book.items():
     if re.search(param.lower(), key.lower()) or re.search(param, val):
-      print key, val
-
+      print(key, val)
 
 def add(aname, aphone):
   db_w = getConf()
@@ -20,7 +19,7 @@ def add(aname, aphone):
     add_book[aname] = aphone
     db_w.updateDB(add_book)
   db_contents = db_w.getDB()
-  print "DB contents: \n", db_contents.items()
+  print("DB contents: \n", db_contents.items())
 
 
 def remove(dname):
@@ -31,20 +30,19 @@ def remove(dname):
   else:
     value = add_book.pop(dname)
     db_w.updateDB(add_book)
-    print value + ' deleted'
+    print(value + ' deleted')
 
 def getConf():
-  config = ConfigParser.ConfigParser()
-  config.read(r'D:\python\mypy\config.cfg')
+  config = configparser.ConfigParser()
+  config.read(r'config.cfg')
   db_type = config.get('DB_File', 'type')
   if db_type == 'pickle':
-    #print 'Pickle'
+    #print('Pickle')
     db_w = __import__('wFiles_pickle')
-    return db_w
   elif db_type == 'csv':
-    #print "Csv"
+    #print("Csv")
     db_w = __import__('wFiles_csv')
-    return db_w
+  return db_w
 
 def main():
   pass
