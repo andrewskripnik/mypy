71345ccb26b4d500ae1d192c441bb90c101d7886
SkAI <andrewskripnik@gmail.com>
2024-12-20 00:12:14 +0200

    Add json compare code

---
+++ compare_json_files/compare.py
@@ -0,0 +1,31 @@
+import json
+import sys
+from deepdiff import DeepDiff
+
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def compare_json(json1, json2):
+    diff = DeepDiff(json1, json2, ignore_order=True)
+    if diff:
+        return diff
+    else:
+        return None
+
+def main(file1, file2):
+    json1 = load_json(file1)
+    json2 = load_json(file2)
+    
+    differences = compare_json(json1, json2)
+    if differences:
+        print("The JSON files are different.")
+        print(differences)
+    else:
+        print("The JSON files are identical.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python compare.py <file1> <file2>")
+    else:
+        main(sys.argv[1], sys.argv[2])
