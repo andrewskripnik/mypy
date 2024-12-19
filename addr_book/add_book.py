a27a6252203fe94d27a0a6f4fea7601737f06a2f
SkAI <andrewskripnik@gmail.com>
2024-12-19 14:12:06 +0200

    Fix indent

--- addr_book/add_book.py
+++ addr_book/add_book.py
@@ -3,37 +3,32 @@ from crud import search, add, remove
 act = input('Choose action: \nsearch \nadd \ndelete \ngetDB \n')
 add_book = {}
 
-
 ######## Main
 def main():
   if act.lower() in ['se', 'sea', 'sear', 'searc', 'search']:
-      sname = input('Name for search please: ')
-      search(sname)
-
+    sname = input('Name for search please: ')
+    search(sname)
   elif act.lower() == 'add':
-      aname = input('Name for update please: ')
-      aphone = input('Phone for update please: ')
-      try:
-          add(aname, aphone)
-      except ValueError:
-          print('Contact exists')
-
+    aname = input('Name for update please: ')
+    aphone = input('Phone for update please: ')
+    try:
+      add(aname, aphone)
+    except ValueError:
+      print('Contact exists')
   elif act.lower() in ['delete','del']:
-      dname = input('Exact name for deletion please: ')
-      try:
-          remove(dname)
-      except ValueError:
-          print('No such contact')
-
+    dname = input('Exact name for deletion please: ')
+    try:
+      remove(dname)
+    except ValueError:
+      print('No such contact')
   elif act == 'getDB':
-      try:
-          print("getDB()")
-      except ValueError:
-          print(ValueError)
-
+    try:
+      print("getDB()")
+    except ValueError:
+      print(ValueError)
   else: print("Choose action: se[arch], add, del[ete], getDB ")
 
 
 if __name__ == "__main__":
-    main()
+  main()
 
